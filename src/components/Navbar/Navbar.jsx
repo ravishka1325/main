ab393506976d1c8103378cd7e53b55dd789fe436
dsathyajith <meeriyagallad@gmail.com>
2025-01-11 03:45:50 +0530

    navbar update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
@@ -6,36 +6,39 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="navbar-container">
         <div className="navbar-content">
-        <div className="logo">
-        <span className="logo-text">CINEX</span>
-        </div>
+          <div className="logo">
+            <span className="logo-text">CINEX</span>
+          </div>
 
           <div className="nav-links">
-            <NavLink href="/" >Home</NavLink>
+            <NavLink href="/">Home</NavLink>
             <NavLink href="/Moviepage">Movies</NavLink>
             <NavLink href="/offers">Offers & Promotions</NavLink>
-            <NavLink href="/">Contact Us</NavLink>
+            <NavLink href="/contact">Contact Us</NavLink>
           </div>
 
           <div className="auth-buttons">
-          <Link to="/RegistrationForm">
-            <button className="login-button">LOGIN</button>
-          </Link>
-          <Link to="/Signup">
-            <button className="signup-button">SIGN UP</button>
-          </Link>
-        </div>
+            <Link to="/RegistrationForm">
+              <button className="login-button">LOGIN</button>
+            </Link>
+            <Link to="/Signup">
+              <button className="signup-button">SIGN UP</button>
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
   );
 };
 
-const NavLink = ({ href, children, active = false }) => {
+const NavLink = ({ href, children }) => {
+  const location = useLocation();
+  const isActive = location.pathname === href;
+
   return (
     <Link
       to={href}
-      className={`nav-link ${active ? 'active' : ''}`}
+      className={`nav-link ${isActive ? "active" : ""}`}
     >
       {children}
     </Link>
